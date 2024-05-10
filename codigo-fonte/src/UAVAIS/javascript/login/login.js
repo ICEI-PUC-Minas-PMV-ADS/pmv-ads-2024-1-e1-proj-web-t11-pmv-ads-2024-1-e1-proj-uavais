5aace2143bf26291d52cf08e529a7554c3b2ae93
fenhli <shorepaes191@proton.me>
2024-05-10 20:07:15 -0300

    pagina de login

---
+++ codigo-fonte/src/UAVAIS/javascript/login/login.js
@@ -0,0 +1,7 @@
+let seuNome = document.getElementById("#input1")
+let nomePlace = document.getElementById("#placeholder1")
+
+
+seuNome.addEventListener('click', function(){
+  nomePlace.textContent = "";
+})
